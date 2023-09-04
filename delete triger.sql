651bbffb9ba26387d1eec945d3c9004f89f12370
DarjaMiljukova <120181585+DarjaMiljukova@users.noreply.github.com>
2023-09-04 10:32:22 +0300

    delete triger.sql

--- delete triger.sql
+++ delete triger.sql
@@ -1,1 +1,15 @@
+--loo triger
+create trigger guestKustutamine
+on guest
+for delete
+as
+insert into logi(kuupaev, kasutaja, andmed, tegevus)
+select getdate(), user,
+concat(deleted.first_name, ', ', deleted.last_name),
+'guest on kustutatud'
+from deleted
 
+--kontroll
+delete from guest where id=1
+select * from guest;
+select * from logi;
